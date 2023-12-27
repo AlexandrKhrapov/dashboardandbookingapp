98cbd3515e943324c49e741e9008651770a9112d
AlexandrKhrapov <155019016+AlexandrKhrapov@users.noreply.github.com>
2023-12-27 23:18:30 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,17 @@
+from dash import Dash, html, dcc
+import dash
+import dash_bootstrap_components as dbc
+
+app = Dash(__name__, pages_folder='pages', use_pages=True, external_stylesheets=[dbc.themes.LUX])
+
+app.layout = html.Div([
+    html.Br(),
+    html.Div(children=[
+        dcc.Link(page['name'], href=page['relative_path'])
+        for page in dash.page_registry.values()
+    ]),
+    dash.page_container
+])
+
+if __name__ == '__main__':
+    app.run(debug=True)
