9e238e8ceedd5dae83c660fde0975fdcf493136f
AlexandrKhrapov <155019016+AlexandrKhrapov@users.noreply.github.com>
2023-12-28 04:35:57 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
+from dash import Dash, html, dcc
+import dash
+import dash_bootstrap_components as dbc
 import os
-from flask import Flask
 
-app = Flask(__name__)
+app = Dash(__name__, pages_folder='pages', use_pages=True, external_stylesheets=[dbc.themes.LUX])
 
+app.layout = html.Div([
+    html.Br(),
+    html.Div(children=[
+        dcc.Link(page['name'], href=page['relative_path'])
+        for page in dash.page_registry.values()
+    ]),
+    dash.page_container
+])
 
-@app.route("/")
-def hello_world():
-    return "Hello!"
-
-
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+if __name__ == '__main__':
+    app.run(port=int(os.environ.get("PORT", 8080)), host='0.0.0.0')
