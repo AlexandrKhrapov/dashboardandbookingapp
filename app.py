6edb1834de18d2b1f3b62184ab5afed8dc400f9c
AlexandrKhrapov <155019016+AlexandrKhrapov@users.noreply.github.com>
2023-12-28 04:53:09 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 from dash import Dash, html, dcc
 import dash
-import os
+import dash_bootstrap_components as dbc
 
-app = Dash(__name__)
+app = Dash(__name__, pages_folder='pages', use_pages=True, external_stylesheets=[dbc.themes.LUX])
 
 app.layout = html.Div([
-    html.H3("IM DEAD")
+    html.Br(),
+    html.Div(children=[
+        dcc.Link(page['name'], href=page['relative_path'])
+        for page in dash.page_registry.values()
+    ]),
+    dash.page_container
 ])
 
 if __name__ == '__main__':
-    app.run(port=int(os.environ.get("PORT", 8080)), host='0.0.0.0')
+    app.run(debug=True)
