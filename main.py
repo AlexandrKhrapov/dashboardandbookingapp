5ecdb2eea481e67c7afb1eee77d308461f68b3b6
AlexandrKhrapov <155019016+AlexandrKhrapov@users.noreply.github.com>
2023-12-28 05:34:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from dash import Dash, html, dcc
 import dash
 import dash_bootstrap_components as dbc
 
-app = Dash(__name__, pages_folder='pages', use_pages=True, external_stylesheets=[dbc.themes.LUX])
+main = Dash(__name__, pages_folder='pages', use_pages=True, external_stylesheets=[dbc.themes.LUX])
 
-app.layout = html.Div([
+main.layout = html.Div([
     html.Br(),
     html.Div(children=[
         dcc.Link(page['name'], href=page['relative_path'])
