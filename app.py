eb2ba7cb693f2624525e3389e964c322f9e61f68
AlexandrKhrapov <155019016+AlexandrKhrapov@users.noreply.github.com>
2023-12-28 03:21:28 +0300

    Update app.py

--- app.py
+++ app.py
@@ -15,4 +15,4 @@ app.layout = html.Div([
 ])
 
 if __name__ == '__main__':
-    app.run(port=int(os.environ.get("PORT", 8080)),host='0.0.0.0')
+    app.run(port=int(os.environ.get("PORT", 8080)), host='0.0.0.0')
